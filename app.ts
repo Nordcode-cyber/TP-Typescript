29d7e3142fb30abe865ca4879767bbf9f3a3341a
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-12-25 22:58:51 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,20 @@
+var msg: string = "hola mundo";
+console.log(msg)
+var nombre: string = 'Nordevelop';
+console.log(nombre);
+//el tipo number no se puede asignar al tipo String
+//typescript
+//typescript es un superset de javascript
+
+(()=>{
+    let b:number =10;
+let c:number =5;
+console.log("la suma de a + b es: "+b+c)
+
+})()
+/* funcion flecha en Typescript
+ * (()=>{
+//cuerpo de la funcion
+ })()
+ * 
+ */
