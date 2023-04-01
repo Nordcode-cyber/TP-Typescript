7aa1d1ee9341bec937a9ec988a3ff694230e6700
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-04-01 13:56:53 -0300

    Add files via upload

---
+++ tuplas/script.js
@@ -0,0 +1,8 @@
+(function () {
+    var lenguajes = ['Kotlin', 120, 'Android', true];
+    console.log(lenguajes);
+    console.table(lenguajes);
+    var naves = ['Navio', 11, 'la mara', 25];
+    console.log(naves);
+    console.table(naves);
+})();
---
+++ tuplas/script.ts
@@ -0,0 +1,9 @@
+(()  => {
+    const lenguajes:[string,number,string, boolean] = ['Kotlin',120,'Android', true]; 
+    console.log(lenguajes);
+    console.table(lenguajes);
+    const naves:[string, number,string, number] = ['Navio',11,'la mara',25];
+    console.log(naves);
+    console.table(naves);
+
+})()
