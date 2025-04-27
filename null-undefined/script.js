720911abfbc6858c3f524f18884199fab3166c48
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-27 20:39:34 -0300

    parsing files

--- null-undefined/script.js
+++ null-undefined/script.js
@@ -1,7 +1,8 @@
 (function () {
     var nada = undefined;
-    console.log(nada);
     var vacio = null;
+    //null !== Undefined
+    console.log(nada);
     console.log(vacio);
     console.log(typeof nada);
     console.log(typeof vacio);
