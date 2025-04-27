49afdfc93bf0053082e22da0442ad3eb50211932
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-27 20:20:07 -0300

    script.ts

--- null-undefined/script.ts
+++ null-undefined/script.ts
@@ -2,7 +2,6 @@
     let nada:undefined = undefined;
      let vacio:null = null;
     //null !== Undefined
-    nada = 2+1;
     console.log(nada);
     console.log(vacio);
     console.log(typeof nada);
