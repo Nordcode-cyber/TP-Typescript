fe30607d5ed8bdcd7b5144b910bc9a4577f6ac83
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-28 22:36:33 -0300

    transpiler metodo.ts

--- clases/metodo.js
+++ clases/metodo.js
@@ -8,17 +8,42 @@
             this.year = year;
             this.language = language;
         }
+        Ide.prototype.descripcion = function () {
+            return "".concat(this.name, " (").concat(this.year, ")(").concat(this.creator, ")");
+        };
         return Ide;
     }());
     var vscode = new Ide('vs code', 'microsoft', true, 2015, 'multilanguage');
     console.log({ vscode: vscode });
     console.table(vscode);
+    console.log(vscode.descripcion());
     console.log('vscode es de tipo: ' + typeof vscode);
     console.log('Ide es de tipo: ' + typeof Ide);
     console.log('nombre del Ide: ' + ' ' + vscode.name);
     console.log('creador del Ide: ', vscode.creator);
-    //  console.log({thor})
-    // console.log(typeof Avenger)
-    // console.log(typeof thor.realName)
-    //  console.log(typeof thor.team)
+    var eclipse = new Ide('eclipse', 'sun microsistem', true, 204, 'Java');
+    console.log({ eclipse: eclipse });
+    console.table(eclipse);
+    console.log(eclipse.descripcion());
+    var So = /** @class */ (function () {
+        function So(name, creator, company, year, pack, model, multiplataforma) {
+            this.name = name;
+            this.creator = creator;
+            this.company = company;
+            this.year = year;
+            this.pack = pack;
+            this.model = model;
+            this.multiplataforma = multiplataforma;
+        }
+        So.prototype.tecnica = function () {
+            return "".concat(this.creator, " (").concat(this.pack, ") (").concat(this.model, ")(").concat(this.company, ")");
+        };
+        return So;
+    }());
+    var linux = new So('gnu/linux', 'richard & Linus', 'project gnu', 1991, 'tar.gz', 'free software', true);
+    //solo se puede acceder a las miembros publicos de las clases
+    console.log({ linux: linux });
+    console.table(linux);
+    console.log(linux.tecnica());
+    console.log(linux.name);
 })();
