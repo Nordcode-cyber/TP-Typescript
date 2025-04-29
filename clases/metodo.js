4ff6429e31964ec49cea7085d1402d1395c54654
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-28 21:45:02 -0300

    parsing Metodos

--- clases/metodo.js
+++ clases/metodo.js
@@ -11,13 +11,14 @@
         return Ide;
     }());
     var vscode = new Ide('vs code', 'microsoft', true, 2015, 'multilanguage');
-    console.log(vscode);
+    console.log({ vscode: vscode });
     console.table(vscode);
-    console.log(typeof vscode);
-    console.log(typeof Ide);
-    // console.log('nombre del superhero: '+' '+ thor.realName)
+    console.log('vscode es de tipo: ' + typeof vscode);
+    console.log('Ide es de tipo: ' + typeof Ide);
+    console.log('nombre del Ide: ' + ' ' + vscode.name);
+    console.log('creador del Ide: ', vscode.creator);
     //  console.log({thor})
-    // console.log(typeof Avenger)node
+    // console.log(typeof Avenger)
     // console.log(typeof thor.realName)
     //  console.log(typeof thor.team)
 })();
--- clases/metodo.ts
+++ clases/metodo.ts
@@ -1,9 +1,9 @@
 (() =>{
     class Ide {
-        private name:string;
-        private creator:string;
-        private Gui:boolean;
-        private year:number;
+        public name:string;
+        public creator:string;
+        public Gui:boolean;
+        private  year:number;
         private language:string
         constructor(name:string,creator:string,Gui:boolean,year:number,language:string){
             /*operador this* */
@@ -13,16 +13,25 @@
             this.year=year;
             this.language=language;
         }
+        descripcion(){
+            return `${this.name} (${this.creator})`
+        }
+
      }
     
     const vscode: Ide =new Ide('vs code','microsoft',true,2015,'multilanguage');
-    console.log(vscode)
+    console.log({vscode})
     console.table(vscode)
-    console.log(typeof vscode)
-    console.log(typeof Ide)
-  // console.log('nombre del superhero: '+' '+ thor.realName)
- //  console.log({thor})
-  // console.log(typeof Avenger)
-  // console.log(typeof thor.realName)
- //  console.log(typeof thor.team)
+    console.log(vscode.descripcion())
+   /* console.log('vscode es de tipo: '+typeof vscode)
+    console.log('Ide es de tipo: '+typeof Ide)
+    console.log('nombre del Ide: '+' '+ vscode.name)
+    console.log('creador del Ide: ',vscode.creator)
+    const eclipse: Ide =new Ide('eclipse','sun microsistem',true,204,'Java');
+    console.log({eclipse})
+    console.table(eclipse)
+    console.log('creador de eclipse es: ',eclipse.creator)
+   * 
+   */
+
 })()
